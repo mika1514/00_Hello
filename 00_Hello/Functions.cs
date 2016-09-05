168f9f84447d12d510e9f2145f2379b1c4977f5e
Mikas Kyed <mikaskyed@hotmail.com>
2016-09-05 09:55:35 +0200

    Created the Function Class

--- 00_Hello/Functions.cs
+++ 00_Hello/Functions.cs
@@ -1,6 +1,15 @@
 ﻿namespace _00_Hello
 {
-    internal class Functions
+    public class Functions
     {
+        public static string Hello()
+        {
+            return "hello";
+        }
+        public static string Hello(string v)
+        {
+            return "hello, Fred";
+        }
+
     }
 }
